66e146b887426f1c88d9b5aaa5b7faaaf00e1475
JoranDePessemier <joran.de.pessemier@student.howest.be>
2023-06-21 17:15:53 +0200

    ADD Particles en tussenstuk

--- Assets/Scripts/DungeonButterflies/ClickButterflyDungeon.cs
+++ Assets/Scripts/DungeonButterflies/ClickButterflyDungeon.cs
@@ -4,5 +4,19 @@ using UnityEngine;
 
 public class ClickButterflyDungeon : BaseButterflyDungeon
 {
+    private int _clickCounter;
 
+    [SerializeField]
+    private int _amountToClick;
+
+    protected override void Clicked()
+    {
+        base.Clicked();
+        _clickCounter++;
+
+        if(_clickCounter >= _amountToClick)
+        {
+            Completed();
+        }
+    }
 }
